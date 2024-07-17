ea0a0844db370a3b5fe73c49223ae4a5ef5aecaa
johnlivingprooff <johnlivingprooff@gmail.com>
2024-07-17 14:09:31 +0200

    some needed clean up

---
+++ client-side/src/pages/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/HeaderTwo';
+import Footer from '../components/Footer';
+
+function Dashboard() {
+    return (
+        <div>
+            <Header />
+            <h1>Dashboard</h1>
+            <Footer />
+        </div>
+    );
+}
+
+export default Dashboard;

a680ede0bdb25ea9b211d89a598c8619596d8b9e
johnlivingprooff <johnlivingprooff@gmail.com>
2024-09-19 21:00:28 +0200

    fixed?

--- client-side/src/pages/DashReports.jsx
+++ client-side/src/pages/DashReports.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
+import Header from '../components/DashHead';
+import Footer from '../components/Footer';
+
 function DashReports() {
     return (
         <div>
+            <Header />
             Hello Reports
+            <Footer />
         </div>
     );
 }
